28bb2d5ccb4d38bd2c801d617a30b3a78e194068
Joseph D Hughes <jdhughes@usgs.gov>
2022-01-05 17:29:25 -0600

    ci: working on CI

--- usgsLaTeX/install.sh
+++ usgsLaTeX/install.sh
@@ -8,7 +8,12 @@ echo "\nCurrent directory:\n  " $CURRENT
 TEXROOT=`kpsewhich -var-value TEXMFHOME`
 echo "\nLaTeX home directory:\n  " $TEXROOT
 
-echo "\rcopying USGS LaTeX style files..."
+echo "\nmaking a few directories if they do not exist..."
+mkdir -p $TEXROOT/fonts
+mkdir -p $TEXROOT/tex
+mkdir -p $TEXROOT/dvips
+
+echo "\ncopying USGS LaTeX style files..."
 cp -R fonts $TEXROOT/.
 cp -R tex $TEXROOT/.
 cp -R dvips $TEXROOT/.
