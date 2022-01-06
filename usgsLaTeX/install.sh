8cfaa87cd8f4b2cc2e39bae462427a32c94d197e
Joseph D Hughes <jdhughes@usgs.gov>
2022-01-06 09:35:00 -0600

    ci: working on CI

--- usgsLaTeX/install.sh
+++ usgsLaTeX/install.sh
@@ -8,23 +8,23 @@ echo "Current directory:  " $CURRENT
 TEXROOT=`kpsewhich -var-value TEXMFHOME`
 echo "LaTeX home directory:  " $TEXROOT
 
-echo "making a few directories if they do not exist..."
+echo "Making a few directories if they do not exist..."
 mkdir -p $TEXROOT/fonts
 mkdir -p $TEXROOT/tex
 mkdir -p $TEXROOT/dvips
 
-echo "copying USGS LaTeX style files..."
+echo "Copying USGS LaTeX style files..."
 cp -R fonts $TEXROOT/.
 cp -R tex $TEXROOT/.
 cp -R dvips $TEXROOT/.
 
-echo "installing Univers font..."
+echo "Installing Univers font..."
 cd $TEXROOT/dvips/funivers
 texhash
 updmap -sys --enable Map=funivers.map
 updmap-sys
 
-echo "returning to the starting directory:  " $CURRENT
+echo "Returning to the starting directory:  " $CURRENT
 cd $CURRENT
 
 LOCATION=`kpsewhich usgsreporta.sty`
